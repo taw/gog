c2fd6c33c3b2b51af58482547245f81bfb246fe2
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2014-04-29 14:25:30 +0100

    This unfortunately fails miserably

--- README.md
+++ README.md
@@ -3,9 +3,9 @@ gog
 
 Tools for gog.com
 
-To configure access to gog, please put your login/password in ~/.gogauth, on separate lines:
+To configure access to gog, please put your login/password in `~/.gogauth`, on separate lines:
 
 ```
-username
+email@domain.com
 password
 ```
--- gog.rb
+++ gog.rb
@@ -1,3 +1,6 @@
+require "mechanize"
+require "pathname"
+
 class GOG
   def self.credentials
     @credentials ||= begin
@@ -5,5 +8,18 @@ class GOG
       raise "File #{ptah} doesn't exist" unless path.exist?
       path.readlines.map(&:chomp).grep(/\S+/)[0,2]
     end
+  end
+
+  def initialize
+    @agent = Mechanize.new
+    @agent.log = Logger.new("mechanize.log")
+    @credentials = GOG.credentials
+  end
+
+  def login!
+    page = @agent.get "http://www.gog.com/"
+    form = page.form("login_form")
+    form.log_email = GOG.credentials[0]
+    form.log_password = GOG.credentials[1]
   end
 end
